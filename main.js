ce1f156ae181d75b68d60517d78b36bdcc06223f
anderutan <52438782+anderutan@users.noreply.github.com>
2023-12-13 11:33:28 +0800

    Initialize game variables with function

--- main.js
+++ main.js
@@ -23,5 +23,22 @@ form.addEventListener('submit', (event) => {
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
   document.querySelector('.modal-wrapper').setAttribute('hidden', true);
-  console.log(data);
+  initializeGame(data);
 });
+
+const initializeVariables = (data) => {
+  data.choice = +data.choice;
+  data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  data.player1 = 'X';
+  data.player2 = 'O';
+  data.round = 0;
+  data.currentPlayer = 'X';
+  data.gameOver = false;
+};
+
+const initializeGame = (data) => {
+  // initialize game variables
+  initializeVariables(data);
+  console.log(data);
+  //add event listener to the gameboard
+};
