c69cdba87d14f7e1eeac623fdd4d9a3b0627f273
anderutan <52438782+anderutan@users.noreply.github.com>
2023-12-13 12:27:12 +0800

    started playmove function to play a round

--- main.js
+++ main.js
@@ -49,4 +49,26 @@ const initializeGame = (data) => {
   initializeVariables(data);
 
   //add event listener to the gameboard
+  addEventListenerToGameBoard(data);
+};
+
+const playMove = (box, data) => {
+  // is game over? if game over, don't do anything
+  if (data.gameOver || data.round > 8) {
+    return;
+  }
+  // check if game box has a letter in it, if so, don't do anything
+  if (data.board[box.id] === 'X' || data.board[box.id] === 'O') {
+    return;
+  }
+
+  //adjust the DOM for player move, and then check win conditions
+  data.board[box.id] = data.currentPlayer;
+  box.textContent = data.currentPlayer;
+  box.classList.add(data.currentPlayer === 'X' ? 'player1' : 'player2');
+  //increase the round #
+  data.round++;
+  console.log(box, data);
+
+  //check end condition
 };
