afecd3d2449059ed37e40eaebcece1c380a3aa64
anderutan <52438782+anderutan@users.noreply.github.com>
2023-12-13 11:22:37 +0800

    event listener on form for user data to initialize game

---
+++ main.js
@@ -0,0 +1,27 @@
+//first, we need to attach event (submit) listener to the form to get user data
+
+// attach event (click) listener to each 'game box'
+
+// next, initialize the game
+
+// next, we need to check which gamemode we're playing
+
+// we need to set win conditions
+
+// we need to determine current player
+
+// after each move, check win conditions and if not met, set other player as active
+
+// human vs human, next implement easy ai, next impossible ai
+
+const form = document.querySelector('#myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  //initialize user form data
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  document.querySelector('.modal-wrapper').setAttribute('hidden', true);
+  console.log(data);
+});
